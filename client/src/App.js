dd42142fb67dd4c6f3b8347f494ca28417ddec18
aidanflinton <69019017+aidanflinton@users.noreply.github.com>
2022-05-31 13:29:10 -0400

    navbar nicer (some styling) and component folders more organized

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,50 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, Routes, Route } from "react-router-dom";
+import './App.css'
+
+import Discover from "./components/discover/Discover";
+import Forum from './components/forum/Forum';
+import Home from './components/Home';
+import Inbox from './components/inbox/Inbox';
+import LikedSongs from './components/userinfo/LikedSongs';
+import TopArtists from './components/userinfo/TopArtists';
+import TopSongs from "./components/userinfo/TopSongs";
+import UserProfile from './components/userinfo/UserProfile';
+import Login from './components/userinfo/Login';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
-    <div>
-      <h1>Spotify Social</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/discover">Discover</Link> |{" "}
-        <Link to="/forum">Forum</Link>  |{" "}
-        <Link to="/home">Home</Link>  |{" "}
-        <Link to="/inbox">Inbox</Link> |{" "}
-        <Link to="/liked">Liked Songs</Link> |{" "}
-        <Link to="/artists">Top Artists</Link> |{" "}
-        <Link to="/songs">Top Songs</Link> |{" "}
-        <Link to="/profile">Profile</Link>
-      </nav>
-      <Outlet />
+    <>
+    <div className="App">
+      <h1>
+        Spotify Social
+      </h1>
+    
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Home />} />
+        <Route path="discover" element={<Discover />} />
+        <Route path="forum" element={<Forum />} />
+        <Route path="inbox" element={<Inbox />} />
+        <Route path="liked" element={<LikedSongs />} />
+        <Route path="artists" element={<TopArtists />} />
+        <Route path="songs" element={<TopSongs />} />
+        <Route path="profile" element={<UserProfile />} />
+        <Route path="login" element={<Login />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem 0" }}>
+              <h2>404 not found</h2>
+              <p>
+                  <Link to="/">This link can take you back home though</Link>
+              </p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
     </div>
+    </>
   );
 }
